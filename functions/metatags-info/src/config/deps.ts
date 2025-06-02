ceb10e1f7df33742f2db1c7e3d758c2ac1754084
Hoang Hoa <44923253+hoanghoa12345@users.noreply.github.com>
2025-06-02 19:21:16 +0700

    Fix function dependencies

--- functions/metatags-info/src/config/deps.ts
+++ functions/metatags-info/src/config/deps.ts
@@ -6,8 +6,8 @@ export { oakCors } from "https://deno.land/x/cors/mod.ts";
 export { yellow, green, red } from "jsr:@std/fmt/colors";
 export { DOMParser, Element } from "jsr:@b-fuze/deno-dom";
 
-export { createClient } from "jsr:@supabase/supabase-js@2";
-export type { SupabaseClient } from "jsr:@supabase/supabase-js@2";
+export { createClient } from "jsr:@supabase/supabase-js@2.49.8";
+export type { SupabaseClient } from "jsr:@supabase/supabase-js@2.49.8";
 
 export { z } from "npm:zod@3.21.4";
 export { Pool } from "jsr:@db/postgres";
